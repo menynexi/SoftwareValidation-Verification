56a6f4c41a0003cfaebbf676b9c7c0faa06877f7
Edgar Del Rivero <108140039+edgarrivero33@users.noreply.github.com>
2023-04-22 13:47:20 -0600

    subtraction code for calculator
    all test cases pass

--- subtraction.py
+++ subtraction.py
@@ -1,29 +1,26 @@
+# Preconditions: Expression must be between two numbers only
 def subtraction(str):
-    print("number was subtracted")
+    str = str.replace('- -', '+')
+    print("number was added")
     expression = str.split(" ")
     if len(expression) >= 4 and expression[0] == '-':
         temp = expression[0] + expression[1]
         num1 = float(temp)
         num2 = float(expression[-1])
-    elif(expression[2] == '-' and len(expression[1]) == 0):
-        temp = expression[0] + expression[2] + expression[3]
-        num1 = float(temp)
-        num2 = float(expression[-1])
     elif(expression[2] == '-'):
         temp = expression[-2] + expression[-1]
-        num1 = float(expression[0])
-        num2 = float(temp)
+        num1 = float(temp)
+        num2 = float(expression[0])
     else:
         num1 = float(expression[0])
         num2 = float(expression[-1])
     return num1 - num2
 
-
 def testcases():
-    assert subtraction("2 - 2") == 0.0
+    assert subtraction("2 - 2") == 0
+    assert subtraction("2 - - 2") == 0
     assert subtraction("- 2 - 2") == -4.0
-    assert subtraction("2 - - 2") == 4.0
-    assert subtraction("0 - - 2") == 2.0
+    assert subtraction("0 - 2") == -2.0
     print("all test cases passed")
-    
+
 testcases()
