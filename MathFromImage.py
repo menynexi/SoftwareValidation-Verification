a58e6a13b307c199b2c1c057104fffb0ee77d43a
Edgar Del Rivero <108140039+edgarrivero33@users.noreply.github.com>
2023-04-23 22:41:08 -0600

    removed spacing

--- MathFromImage.py
+++ MathFromImage.py
@@ -1,9 +1,6 @@
 import easyocr
 reader = easyocr.Reader(['en'], gpu= True)
 results = reader.readtext('math.jpg',allowlist='0123456789 %/*-+')
-
-# print(results)
-
 text = ''
 for result in results:
     text+= result[1] + ''
